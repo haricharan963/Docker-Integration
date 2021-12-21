1165b344e755c7325e0dd4d7626e77192dac805c
First Last <haricharanallam@ip-172-31-27-136.ec2.internal>
2021-12-21 08:43:52 +0000

    third commit

--- src/test/java/com/mindtree/docker_jenkins_integration/AppTest.java
+++ src/test/java/com/mindtree/docker_jenkins_integration/AppTest.java
@@ -3,5 +3,7 @@ package com.mindtree.docker_jenkins_integration;
 
 public class AppTest {
     
-  
+  public static void main(String[] args) {
+	System.out.println("Integrating jenkins and docker");
+}
 }
